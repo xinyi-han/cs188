f9c15642b8db3571b4f35a6ce9de7dcb6ba76d2a
Xinyi Han <xinyi.han98@outlook.com>
2022-09-28 17:36:03 +1000

    Complete Q7 Epsilon Greedy

--- proj3-reinforcement/qlearningAgents.py
+++ proj3-reinforcement/qlearningAgents.py
@@ -101,7 +101,12 @@ class QLearningAgent(ReinforcementAgent):
         legalActions = self.getLegalActions(state)
         action = None
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if len(legalActions) > 0:
+            if util.flipCoin(self.epsilon):
+                action = random.choice(legalActions)
+            else:
+                action = self.getPolicy(state)
+        # util.raiseNotDefined()
 
         return action
 
